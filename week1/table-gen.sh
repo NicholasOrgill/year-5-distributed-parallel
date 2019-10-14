270405f3b5ca58b3b7d3c67b1d923a707ba79157
Nicholas Orgill <nicholas@mbp.local>
2019-10-14 11:51:25 +0100

    Sorted out file structure

--- week1/table-gen.sh
+++ week1/table-gen.sh
@@ -8,5 +8,17 @@ do
     echo -n "C2-imgflipP V $i "
     ./C2-imflipP/Debug/C2-imflipP ./C2-imflipP/images/townSquare.bmp ./C2-imflipP/images/townSquareV.bmp V $i | egrep -o "[0-9]\.[0-9]+ ms"
     echo -n "C2-imgflipP H $i "
-    ./C2-imflipP/Debug/C2-imflipP ./C2-imflipP/images/townSquare.bmp ./C2-imflipP/images/townSquareV.bmp H $i | egrep -o "[0-9]\.[0-9]+ ms"
+    ./C2-imflipP/Debug/C2-imflipP ./C2-imflipP/images/townSquare.bmp ./C2-imflipP/images/townSquareH.bmp H $i | egrep -o "[0-9]\.[0-9]+ ms"
+done
+
+for i in {1..24}
+do
+    echo -n "C3-imgflipPM V $i "
+	./C3-imflipPM/Debug/C3-imflipPM ./C3-imflipPM/images/townSquare.bmp ./C3-imflipPM/images/townSquareV.bmp V $i | egrep -o "[0-9]\.[0-9]+ ms"
+	echo -n "C3-imgflipPM H $i "
+	./C3-imflipPM/Debug/C3-imflipPM ./C3-imflipPM/images/townSquare.bmp ./C3-imflipPM/images/townSquareH.bmp H $i | egrep -o "[0-9]\.[0-9]+ ms"
+	echo -n "C3-imgflipPM W $i "
+	./C3-imflipPM/Debug/C3-imflipPM ./C3-imflipPM/images/townSquare.bmp ./C3-imflipPM/images/townSquareW.bmp W $i | egrep -o "[0-9]\.[0-9]+ ms"
+	echo -n "C3-imgflipPM I $i "
+	./C3-imflipPM/Debug/C3-imflipPM ./C3-imflipPM/images/townSquare.bmp ./C3-imflipPM/images/townSquareI.bmp I $i | egrep -o "[0-9]\.[0-9]+ ms"
 done
